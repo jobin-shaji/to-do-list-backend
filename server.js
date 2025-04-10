92af99d38cfc6ee8d5b2f5aa4e30e2d1cdecb298
Jobin <jobinshaji111@gmail.com>
2025-04-10 09:50:16 +0530

    env hide

--- server.js
+++ server.js
@@ -103,5 +103,5 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on ${MONGO_URI}:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
